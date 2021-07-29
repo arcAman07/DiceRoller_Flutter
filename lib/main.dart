d9988fb06650831e897c130931db488ebafe8080
Aman Sharma <amananytime07@gmail.com>
2021-07-29 21:13:09 +0530

    The first commit for this project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,7 +7,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(child: Text('Random Dice')),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -14,10 +15,53 @@ void main() {
     ),
   );
 }
+class DicePage extends StatefulWidget {
 
-class DicePage extends StatelessWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber=5;
+  int rightDiceNumber=2;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  Random random = new Random();
+                  int randomNumber = random.nextInt(6)+1;
+                  leftDiceNumber= randomNumber;
+                  print("The current left dice number is $leftDiceNumber");
+
+                });
+
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  Random random = new Random();
+                  int newNumber = random.nextInt(6)+1;
+                  rightDiceNumber= newNumber;
+                  print("The current right dice number is $rightDiceNumber");
+
+                });
+
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
